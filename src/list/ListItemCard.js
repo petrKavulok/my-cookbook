cc2841929425b538ef094887ce2778f479236e65
petrKavulok <petr.kavulok@gmail.com>
2023-05-29 10:02:05 +0200

    List Item Card init

--- src/list/ListItemCard.js
+++ src/list/ListItemCard.js
@@ -1,7 +1,28 @@
+import logo from '../assets/ackee_placeholder.png';
+import DateTime from '../components/DateTime';
 
+const ListItemCard = ({recipe}) => {
+    // const placeholder = import('../assets/placeholder.jpeg')
 
-const ListItemCard = (props) => {
-
+    return (
+        <div className='recipe'>
+            <img
+                src={recipe.img ?? logo}
+                alt={recipe.name}
+                className='list_item_cover_pic'>
+            </img>
+            <div className='list_item_info'>
+                <h2>{recipe.name}</h2>
+                <div>
+                    {recipe.score}
+                </div>
+                <div className='recipe_duration'>
+                    <i className='cil-clock pr-2'></i>
+                    <span>{recipe.duration} min.</span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ListItemCard;
