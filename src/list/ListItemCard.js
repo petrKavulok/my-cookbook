5b9537799b3d6a4cf43bbcca086059c1fc53b1f3
petrKavulok <petr.kavulok@gmail.com>
2023-05-30 21:53:30 +0200

    indentation using tabs

--- src/list/ListItemCard.js
+++ src/list/ListItemCard.js
@@ -1,28 +1,29 @@
 import logo from '../assets/ackee_placeholder.png';
-import DateTime from '../components/DateTime';
+import { Link } from 'react-router-dom';
 
 const ListItemCard = ({recipe}) => {
-    // const placeholder = import('../assets/placeholder.jpeg')
 
-    return (
-        <div className='recipe'>
-            <img
-                src={recipe.img ?? logo}
-                alt={recipe.name}
-                className='list_item_cover_pic'>
-            </img>
-            <div className='list_item_info'>
-                <h2>{recipe.name}</h2>
-                <div>
-                    {recipe.score}
-                </div>
-                <div className='recipe_duration'>
-                    <i className='cil-clock pr-2'></i>
-                    <span>{recipe.duration} min.</span>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className='recipe'>
+			<img
+				src={recipe.img ?? logo}
+				alt={recipe.name}
+				className='list_item_cover_pic'>
+			</img>
+			<div className='list_item_info'>
+				<Link to={`/detail/${recipe.id}`}>
+					<h2>{recipe.name}</h2>
+				</Link>
+				<div>
+					{recipe.score}
+				</div>
+				<div className='recipe_duration'>
+					<i className='cil-clock pr-2'></i>
+					<span>{recipe.duration} min.</span>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ListItemCard;
