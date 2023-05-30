f747711c49015f25f6350f07f4a2f4246356601a
petrKavulok <petr.kavulok@gmail.com>
2023-05-30 21:50:49 +0200

    router init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
-
-import './App.scss';
-import List from './list';
+import { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Header } from './containers';
+import logo from './assets/ackee_placeholder.png';
+import List from './list';
+import RecipeDetailContainer from './detail/RecipeDetailContainer';
+import RecipeCreateContainer from './detail/RecipeCreateContainer';
+import './App.scss';
 
 function App() {
+
+	const routes = [
+		{
+			path: '/', // Url path
+			exact: true,        // Whether path must be matched exactly
+			name: 'Home',  // Route name
+			component: <List /> // Component to be rendered
+		},
+		{
+			path: '/detail/:id',
+			exact: true,
+			name: 'Recipe Detail',
+			component: <RecipeDetailContainer />
+		},
+		{
+			path: '/new-recipe',
+			exact: true,
+			name: 'New Recipe',
+			component: <RecipeCreateContainer />
+		}
+	]
 return (
-	<div className="App">
-		<Header />
-		<List />
-	</div>
+		<Router>
+			<div className="App">
+				<Header />
+				<Suspense fallback={<img src={logo}/>}>
+					<Routes>
+						{routes.map((route, idx) => {
+							return (
+								<Route
+								key={idx}
+								path={`${route.path}`}
+								exact={route.exact}
+								name={route.name}
+								// element={props => (
+								// 		<>
+								// 					<route.component {...props} {...route.props} />
+								// 		</>
+								// 	)}
+									element={route.component}
+									/>
+									)
+								})}
+					</Routes>
+				</Suspense>
+					{/* <List /> */}
+			</div>
+		</Router>
 );
 }
 
