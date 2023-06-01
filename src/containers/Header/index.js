55bde2920ed852b65dce6b7f0b3d48d29817356d
petrKavulok <petr.kavulok@gmail.com>
2023-06-01 22:31:14 +0200

    props in header

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -3,7 +3,7 @@ import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import './header.scss';
 
-const Header = () => {
+const Header = (props) => {
 
 	// const history = useHistory();
 
